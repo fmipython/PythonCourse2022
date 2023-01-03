4f19b5d9c881b799a4c4d39e59beef66d43a39a2
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-03 13:44:30 +0200

    Git sync

--- 16 - Web programming/examples/login_page.py
+++ 16 - Web programming/examples/login_page.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('fancy_index.html', title='Home', user='Guest')
-
-
-@app.route("/login")
-def login(message=None):
-    return render_template('login.html', title='Login', message=message)
+    return render_template('index.html', title='Home', user='Guest')
 
 
 @app.route("/login_action", methods=['POST'])
@@ -18,10 +13,18 @@ def login_action():
         return redirect(url_for('/login', message='Invalid method'))
 
     if request.form['username'] == 'admin' and request.form['password'] == 'admin':
-        return redirect(url_for('user/admin'))
+        username = request.form['username']
+        return redirect(url_for('user', name=username))
     else:
-        return redirect(url_for('login', message='Wrong username or password'))
+        return redirect(url_for('login', message='Invalid username or password'))
 
-@app.route("/user/<username>")
-def user_page(username):
-    return render_template('index.html', title='User', user=username)
+
+@app.route("/login")
+def login(message=None):
+    print(message)
+    return render_template('login.html', title='Login', message=message)
+
+
+@app.route("/user/<name>")
+def user_page(name):
+    return render_template('user.html', title='User', user=name)
